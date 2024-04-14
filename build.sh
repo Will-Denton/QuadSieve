4630ea03404f5080a71823ba119e89eefc4eff2c
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-04-14 19:26:41 -0400

    Add time to build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 set -e
 
-gcc quadratic_sieve.c -o quadratic_sieve -I$(brew --prefix gmp)/include -L$(brew --prefix gmp)/lib -lgmp
-./quadratic_sieve
+gcc quadratic_sieve.c -o quadratic_sieve -I$(brew --prefix gmp)/include -L$(brew --prefix gmp)/lib -lgmp -O3
+time ./quadratic_sieve
