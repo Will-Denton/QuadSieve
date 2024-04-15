c34cf2e965786d6f9c1c99ad8bd6889e4236dfa4
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-04-14 20:50:56 -0400

    Clean up

--- quadratic_sieve.c
+++ quadratic_sieve.c
@@ -4,59 +4,49 @@
 #include <math.h>
 #include <stdbool.h>
 
-int* sieve_of_eratosthenes(int B, int* factor_base_length) {
-    // Dynamic array to store primes
-    int capacity = 1;
-    int* factor_base = (int*)malloc(capacity * sizeof(int));
-    *factor_base_length = 0;
-
-    // Allocate memory for prime check array
-    bool* is_primes = (bool*)malloc((B + 1) * sizeof(bool));
-    for(int i = 0; i <= B; i++) {
-        is_primes[i] = true;
+int* sieve_of_eratosthenes(int B, int* num_primes_under_B) {
+    // Initialize array of booleans
+    bool* is_prime = malloc((B + 1) * sizeof(bool));
+    for (int i = 0; i < B + 1; i++) {
+        is_prime[i] = true;
     }
-    is_primes[0] = false;
-    is_primes[1] = false;
+    is_prime[0] = is_prime[1] = false;
 
     // Sieve of Eratosthenes
-    for(int i = 2; i < ceil(sqrt(B+1)); i++) {
-        if(is_primes[i]) {
-            // Dynamically resize factor_base if necessary
-            if (*factor_base_length >= capacity) {
-                capacity *= 2;
-                factor_base = (int*)realloc(factor_base, capacity * sizeof(int));
-            }
-
-            factor_base[*factor_base_length] = i;
-            (*factor_base_length)++;
-
-            for(int j = i * i; j <= B; j += i) {
-                is_primes[j] = false;
+    for (int i = 2; i < ((int)sqrt(B))+1; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j < B+1; j += i) {
+                is_prime[j] = false;
             }
         }
     }
 
-    for (int i = ceil(sqrt(B+1)); i <= B; i++) {
-        if(is_primes[i]) {
-            // Dynamically resize factor_base if necessary
-            if (*factor_base_length >= capacity) {
-                capacity *= 2;
-                factor_base = (int*)realloc(factor_base, capacity * sizeof(int));
-            }
-
-            factor_base[*factor_base_length] = i;
-            (*factor_base_length)++;
+    // Count number of primes under B
+    *num_primes_under_B = 0;
+    for (int i = 2; i < B+1; i++) {
+        if (is_prime[i]) {
+            (*num_primes_under_B)++;
         }
     }
 
-    free(is_primes);
+    // Get primes
+    int* primes_under_B = malloc(*num_primes_under_B * sizeof(int));
+    int primes_index = 0;
+    for (int i = 2; i < B + 1; i++) {
+        if (is_prime[i]) {
+            primes_under_B[primes_index++] = i;
+        }
+    }
 
-    return factor_base;
+    // Free memory
+    free(is_prime);
+
+    return primes_under_B;
 }
 
 void sieve(mpz_t n, int B, int S, mpz_t* factor1, mpz_t* factor2) {
     /*
-        ceil(sqrt(n))
+    root_n = math.ceil(math.sqrt(n))
     */
     mpz_t root_n;
     mpz_init(root_n);
@@ -71,18 +61,16 @@ void sieve(mpz_t n, int B, int S, mpz_t* factor1, mpz_t* factor2) {
     mpf_clear(n_f);
 
     /*
-        sieve_of_eratosthenes
+    primes_under_B = sieve_of_eratosthenes(B)
     */
-    puts("Creating sieve...");
-    int factor_base_length = 0;
-    int* primes_under_B = sieve_of_eratosthenes(B, &factor_base_length);
+    int num_primes_under_B = 0;
+    int* primes_under_B = sieve_of_eratosthenes(B, &num_primes_under_B);
 
-    printf("Primes up to %d:\n", B);
-    for(int i = 0; i < factor_base_length; i++) {
-        printf("%d ", primes_under_B[i]);
-    }
-    printf("\n");
+    /*
+    factor_base = get_factor_base(primes_under_B, n)
+    */
 
+    // Free memory
     free(primes_under_B);
 
     // Trivial factors (delete later)
@@ -93,12 +81,12 @@ void sieve(mpz_t n, int B, int S, mpz_t* factor1, mpz_t* factor2) {
 int main() {
     mpz_t n;
     mpz_init(n);
-    mpz_set_str(n, "16921456439215439701", 10); // base 10
+    mpz_set_str(n, "46839566299936919234246726809", 10); // base 10
 
     int B = 15000;
     int S = 15000000;
 
-    // Factors of n
+    // Nontrivial factors of n
     mpz_t factor1;
     mpz_init(factor1);
     mpz_t factor2;
