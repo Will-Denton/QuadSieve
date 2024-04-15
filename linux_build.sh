a075e6ab5a76db55411366c4f30a514a0f4a4334
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-04-15 15:10:17 -0400

    Add glib to linux build

--- linux_build.sh
+++ linux_build.sh
@@ -1,4 +1,4 @@
 set -e
 
-gcc quadratic_sieve.c -o quadratic_sieve -O3 -lgmp -lm
+gcc quadratic_sieve.c -o quadratic_sieve -O3 -lgmp -lm `pkg-config --cflags --libs glib-2.0`
 valgrind ./quadratic_sieve
