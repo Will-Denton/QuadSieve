d859a2c0b10d62afa1e6c72a3928b2ca7356dc6d
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-04-15 09:19:09 -0400

    Implement shanks_tonelli

--- quadratic_sieve.c
+++ quadratic_sieve.c
@@ -87,16 +87,21 @@ int* get_factor_base(int* primes, int num_primes, mpz_t n, int* factor_base_size
     return factor_base;
 }
 
+// Computes the ceiling of the square root of n
+void ceil_sqrt(mpz_t result, const mpz_t n) {
+    mpz_init(result);
+    mpz_sqrt(result, n);
+    if (mpz_perfect_square_p(n) == 0) {  // If n is not a perfect square
+        mpz_add_ui(result, result, 1);
+    }
+}
+
 double* get_sieve_log(int S, mpz_t n) {
     puts("Creating Sieve Log...");
 
     // root_n = np.float64(np.ceil(math.sqrt(n)))
     mpz_t root_n_mpz;
-    mpz_init(root_n_mpz);
-    mpz_sqrt(root_n_mpz, n);
-    if (mpz_perfect_square_p(n) == 0) { // if n is not a perfect square
-        mpz_add_ui(root_n_mpz, root_n_mpz, 1);
-    }
+    ceil_sqrt(root_n_mpz, n);
     double root_n = mpz_get_d(root_n_mpz);
 
     // Create sieve
@@ -114,6 +119,116 @@ double* get_sieve_log(int S, mpz_t n) {
     return sieve;
 }
 
+void shanks_tonelli(mpz_t n, int p, int *root_mod_p_1, int *root_mod_p_2) {
+    // calculate the square roots of n mod p
+    // DID THIS https://en.wikipedia.org/wiki/Tonelli%E2%80%93Shanks_algorithm
+    if (p == 2) {
+        *root_mod_p_1 = 1;
+        return;
+    }
+
+    mpz_t Q, z, c, t, R, tmp, p_mpz, b;
+    mpz_inits(Q, z, c, t, R, tmp, p_mpz, b, NULL);
+    mpz_set_ui(p_mpz, p);
+
+    // 1. find S and Q such that p - 1 = Q * 2^S
+    mpz_sub_ui(Q, p_mpz, 1);
+    unsigned long S = 0;
+    while (mpz_even_p(Q)) {
+        mpz_fdiv_q_2exp(Q, Q, 1);
+        S++;
+    }
+
+    // 2. find a quadratic non-residue z
+    mpz_set_ui(z, 2);
+    while (quadratic_residue(z, p) != p - 1) {
+        mpz_add_ui(z, z, 1);
+    }
+
+    // 3. initialize M, c, t, R
+    unsigned long M = S;
+    mpz_powm_ui(c, z, mpz_get_ui(Q), p_mpz);
+    mpz_powm_ui(t, n, mpz_get_ui(Q), p_mpz);
+    mpz_add_ui(tmp, Q, 1);
+    mpz_fdiv_q_2exp(tmp, tmp, 1);
+    mpz_powm_ui(R, n, mpz_get_ui(tmp), p_mpz);
+
+    // 4. loop
+    while (1) {
+        if (mpz_cmp_ui(t, 1) == 0) {
+            *root_mod_p_1 = mpz_get_si(R);
+            mpz_sub(tmp, p_mpz, R);
+            *root_mod_p_2 = mpz_get_si(tmp);
+            mpz_clears(Q, z, c, t, R, tmp, p_mpz, b, NULL);
+            return;
+        }
+
+        unsigned long i = 1;
+        mpz_powm_ui(tmp, t, 1 << i, p_mpz); // 2**i = 1 << i
+        while (mpz_cmp_ui(tmp, 1) != 0) {
+            i++;
+            mpz_powm_ui(tmp, t, 1 << i, p_mpz);
+        }
+
+        mpz_powm_ui(b, c, 1 << (M - i - 1), p_mpz);
+        M = i;
+        mpz_powm_ui(c, b, 2, p_mpz);
+
+        // t = (t * pow(b, 2, p)) % p
+        mpz_powm_ui(tmp, b, 2, p_mpz);
+        mpz_mul(t, t, tmp);
+        mpz_mod(t, t, p_mpz);
+
+        mpz_mul(R, R, b);
+        mpz_mod(R, R, p_mpz);
+    }
+}
+
+void sieve_primes_log(mpz_t n, int* factor_base, int factor_base_size, int S, double* log_sieve) {
+    mpz_t root_n;
+    ceil_sqrt(root_n, n);
+
+    for (int i=0; i<factor_base_size; i++) {
+        int p = factor_base[i];
+
+        int root_mod_p_1 = -1;
+        int root_mod_p_2 = -1;
+        shanks_tonelli(n, p, &root_mod_p_1, &root_mod_p_2);
+        if (root_mod_p_1 != -1) {
+            // Calculate x = (root - root_n) % p
+            mpz_t tmp, p_mpz;
+            mpz_inits(tmp, p_mpz, NULL);
+            mpz_set_ui(p_mpz, p);
+            mpz_t root_mod_p_1_mpz;
+            mpz_init_set_ui(root_mod_p_1_mpz, root_mod_p_1);
+            mpz_sub(tmp, root_mod_p_1_mpz, root_n);
+            mpz_mod(tmp, tmp, p_mpz);
+            int x = mpz_get_si(tmp);
+            mpz_clears(root_mod_p_1_mpz, tmp, p_mpz, NULL);
+
+            for (int j=x; j<S; j+=p) {
+                log_sieve[j] -= log(p);
+            }
+        }
+        if (root_mod_p_2 != -1) {
+            // Calculate x = (root - root_n) % p
+            mpz_t tmp, p_mpz;
+            mpz_inits(tmp, p_mpz, NULL);
+            mpz_set_ui(p_mpz, p);
+            mpz_t root_mod_p_2_mpz;
+            mpz_init_set_ui(root_mod_p_2_mpz, root_mod_p_2);
+            mpz_sub(tmp, root_mod_p_2_mpz, root_n);
+            mpz_mod(tmp, tmp, p_mpz);
+            int x = mpz_get_si(tmp);
+            mpz_clears(root_mod_p_2_mpz, tmp, p_mpz, NULL);
+
+            for (int j=x; j<S; j+=p) {
+                log_sieve[j] -= log(p);
+            }
+        }
+
+    }
+}
 
 void sieve(mpz_t n, int B, int S, mpz_t* factor1, mpz_t* factor2) {
     /*
@@ -153,6 +268,7 @@ void sieve(mpz_t n, int B, int S, mpz_t* factor1, mpz_t* factor2) {
     /*
     sieve_primes_log(n, factor_base, S, sieve)
     */
+    sieve_primes_log(n, factor_base, factor_base_size, S, sieve);
 
     // Free memory
     free(primes_under_B);
