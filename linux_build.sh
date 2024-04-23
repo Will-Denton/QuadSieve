f1cfc734110197366a6c01ff09155b4bf6bf142a
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-04-23 10:08:14 -0400

    Update build

--- linux_build.sh
+++ linux_build.sh
@@ -1,4 +1,10 @@
 set -e
 
-gcc quadratic_sieve.c -o quadratic_sieve -O3 -lgmp -lm `pkg-config --cflags --libs glib-2.0`
+gcc quadratic_sieve.c -o quadratic_sieve \
+-Ofast \
+-lgmp \
+-lm \
+`pkg-config --cflags --libs glib-2.0` \
+`pkg-config --cflags --libs mpfr`
+
 valgrind --leak-check=full ./quadratic_sieve
